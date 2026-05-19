22d89cac9c7133d69a7e0676f6ff23b285b27fc4
Saurabh Kumar <Saurabh.Kumar@grapecity.com>
2026-05-19 11:13:22 +0530

    Induce errors

--- vote/app.py
+++ vote/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request, make_response, g
-from redis import Redis
+# from flask import Flask, render_template, request, make_response, g
+# from redis import Redis
 import os
 import socket
 import random
 import json
-import logging
+# import logging
 
 option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
@@ -18,7 +18,7 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+        g.redis = Redis(host=redis, db=0, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
--- worker/Program.cs
+++ worker/Program.cs
@@ -18,12 +18,12 @@ namespace Worker
             {
                 var pgsql = OpenDbConnection("Server=db;Username=postgres;Password=postgres;");
                 var redisConn = OpenRedisConnection("redis");
-                var redis = redisConn.GetDatabase();
+                var redis = redisConn.GetDatabase()
 
                 // Keep alive is not implemented in Npgsql yet. This workaround was recommended:
                 // https://github.com/npgsql/npgsql/issues/1214#issuecomment-235828359
                 var keepAliveCommand = pgsql.CreateCommand();
-                keepAliveCommand.CommandText = "SELECT 1";
+                keepAliveCommand.CommandText = SELECT 1;
 
                 var definition = new { vote = "", voter_id = "" };
                 while (true)
